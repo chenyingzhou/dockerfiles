e43f404a98cbe01417eaf66a28f66a261aa26646
chenyingzhou <echocyz@gmail.com>
2021-07-20 18:38:03 +0800

    版本相同时跳过编译

--- jdk8-mvn-git/start.sh
+++ jdk8-mvn-git/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $GIT_URL =~ ^(git@|http://|https://)(.*):(.*)/(.*)\.git$ ]]; then
+if [[ $GIT_URL =~ ^(git@|http://|https://)(.*)[:/](.*)/(.*)\.git$ ]]; then
   DOMAIN=${BASH_REMATCH[2]}
   AUTHOR=${BASH_REMATCH[3]}
   PROJECT=${BASH_REMATCH[4]}
@@ -21,12 +21,20 @@ ssh-keyscan ${DOMAIN} >/root/.ssh/known_hosts
 cd /app
 git clone ${GIT_URL} 2>/dev/null
 cd ${PROJECT}
-if [ -n "${VERSION}" ]; then
-  git checkout ${VERSION} 2>/dev/null
+if [ -n "${CHECKOUT}" ]; then
+  git checkout ${CHECKOUT} 2>/dev/null
   git pull 2>/dev.null
 fi
 
-mvn package -Dmaven.test.skip=true
+# 若版本与上次相同则跳过编译
+mkdir -p /opt/_build_versions
+HEAD_VERSION=$(git rev-parse HEAD)
+BUILD_VERSION=$(cat /opt/_build_versions/${PROJECT} 2>/dev/null)
+if [ "${HEAD_VERSION}" != "${BUILD_VERSION}" ]; then
+  mvn clean package -Dmaven.test.skip=true
+  echo ${HEAD_VERSION} >/opt/_build_versions/${PROJECT}
+fi
+
 # 以最大体积的.jar文件为可执行的jar包
 jar=$(find /app/${PROJECT} -type f -name '*.jar' -exec stat -c '%s %n' {} \; | sort -nr | head -1 | awk '{print $2}')
 java -Dfile.encoding=UTF-8 -server ${JAVA_OPTS} -jar ${jar}
