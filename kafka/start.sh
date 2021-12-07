82286e6789fde7f1e51e32ca863fd54553f1f390
chenyingzhou <echocyz@gmail.com>
2021-12-08 07:43:06 +0800

    修复kafka镜像局域网不能访问的问题

--- kafka/start.sh
+++ kafka/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+if [ -n "${ADVERTISED_HOST}" ]; then
+  sed -i "s/localhost:9092/${ADVERTISED_HOST}:9092/" /kafka_2.13-3.0.0/config/kraft/server.properties
+fi
 cd /kafka_2.13-3.0.0
 if [ ! -f "/tmp/kraft-combined-logs/meta.properties" ]; then
   uuid=$(./bin/kafka-storage.sh random-uuid)
