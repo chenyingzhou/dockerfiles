e88635dbcb8e4995f14d2a2e5ed25af721900a3e
chenyingzhou <echocyz@gmail.com>
2023-06-13 11:20:07 +0800

    更新dockerfile

--- jdk17-mvn-git/start.sh
+++ jdk17-mvn-git/start.sh
@@ -29,12 +29,15 @@ fi
 # 若版本与上次相同则跳过编译
 mkdir -p /app/_build_versions
 HEAD_VERSION=$(git rev-parse HEAD)
-BUILD_VERSION=$(cat /app/_build_versions/${PROJECT}_${APP_NAME} 2>/dev/null)
+BUILD_VERSION=$(cat /app/_build_versions/${PROJECT} 2>/dev/null)
 if [ "${HEAD_VERSION}" != "${BUILD_VERSION}" ]; then
   mvn clean package -Dmaven.test.skip=true
-  echo ${HEAD_VERSION} >/app/_build_versions/${PROJECT}_${APP_NAME}
+  echo ${HEAD_VERSION} >/app/_build_versions/${PROJECT}
 fi
+
+# 日志目录
+mkdir -p /app/_logs
 
 # 以最大体积的.jar文件为可执行的jar包
 jar=$(find /app/${PROJECT} -type f -name "*${APP_NAME}*.jar" -exec stat -c '%s %n' {} \; | sort -nr | head -1 | awk '{print $2}')
-java -Dfile.encoding=UTF-8 -server ${JAVA_OPTS} -jar ${jar}
+java -Dfile.encoding=UTF-8 -server ${JAVA_OPTS} -jar ${jar} >/app/_logs/${APP_NAME}.log
